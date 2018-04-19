0ec79e55a96b682520a8ec69bc082fcec8ec27d1
BorjaDLT <velasco.6655@gmail.com>
2018-04-19 12:31:46 +0200

    Deleted unnecesary files

--- Uder.m
+++
@@ -1,7 +0,0 @@
-function Uder = Uder(X, indx)
-    Uder = 1;
-    X = [1,X];
-    for ii=1:length(indx)
-        Uder = Uder * X(indx{ii});
-    end
-end
--- Umat2.m
+++
@@ -1,6 +0,0 @@
-function U = Umat2(X, order)
-    U = cell(1,order);
-    for jj=1:order
-        U{jj} = [1, X]; % Add "bias" term
-    end
-end
