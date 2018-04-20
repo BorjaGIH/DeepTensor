6c2149c2b0a736108d447f5fcaa06e7ad916c460
BorjaDLT <velasco.6655@gmail.com>
2018-04-20 12:52:09 +0200

    WIP

--- nnTensor1.m
+++ nnTensor1.m
@@ -3,100 +3,87 @@ rng(1);
 % https://github.com/BorjaGIH/DeepTensor
 % LS_CPD branch
 
-%% Create feature dataset and multivariate output
+%% Create feature dataset and output
 numfeat = 4;             % Number of features. numfeat+1 is the dimension(s) of the tensor
 numpoints = 150;         % Number of datapoints (each datapoint has numfeat values)
-order = 2;               % Order of the tensor. "order" is degree of the polynomial that tensor product achieves
-lambda = 1e8;            % Regularization parameter
-X = randi(10,numpoints,numfeat); % Input data, numpoints x numfeat matrix
-rank = 3;                % rank of the tensor, for constraint/efficient representation
-options.MaxIter = 300;   % optimization iterations
-nonlin = true;           % learned function is nonlinear
+order = 3;               % Order of the tensor. "order" is degree of the polynomial that tensor product achieves
+X = randi(2,numpoints,numfeat); % Input data, numpoints x numfeat matrix
+X = [ones(numpoints,1) X]; % Add bias term
+rank = 3;                % Rank of the tensor, for constraint/efficient representation
+factor = 4;              % factor to multiply tensor, for "good" initial point (temporary)
 
-% Y output. Created as mode-n prod. with tensor
-initvec = repmat(numfeat+1,1,order);
-W = rand(initvec); % Dense random "TRUE" tensor
-
-for ii = 1:numpoints
-    U = Umat2(X(ii,:),order);
-    Ys(ii) = tmprod(W,U,(1:order));
-end
-Ys = Ys';
-
-% Divide dataset for train and test
-sizetr = 0.7;
-sizete = 0.3;
-
-Xtr = X(1:sizetr*numpoints,:);
-Ystr = Ys(1:sizetr*numpoints); % MISO
-
-Xte = X(1:sizete*numpoints,:);
-Yste = Ys(1:sizete*numpoints);
-
-%% Create initial U0
-tic % time
 size_tens = [numpoints repmat(numfeat+1,1,order)];
+U = cpd_rnd(size_tens(2:end),rank);
+W = cpdgen(U);
+
+for ii = 1:numpoints  % Y output. Created as mode-n prod. with tensor
+    Un = Umat2(X(ii,:),order);
+    Y(ii) = tmprod(W,Un,(1:order));
+end
+Y = Y';
+
+%% Create initial U0 random
+tic % time
 U0 = cpd_rnd(size_tens(2:end),rank);
 
 %% Optimization LS-CPD
-optimizer = 'ls-cpd nls';
-A = coeffMatrix(numfeat, order, Xtr); % build A coefficient matrix
-b = Ystr;
+optimizer = 'ls-cpd/nls_gndl';
+A = coeffMatrix(numfeat, order, X); % build A coefficient matrix
+b = Y;
 
-% Solver options
+% Solver options (default algorithm is nls_gndl
 options.Display = true;
 options.TolFun = eps^2;
 options.TolX = eps;
+options.MaxIter = 300; % default 200
 % options.CGMaxIter = prod(size_tens(2:end));
 
 % compute solution
 [Ures,output] = lscpd_nls(A,b,U0,options);
 
-% check error
-% frob(ful(U)-ful(Uest))/frob(ful(U)
-
 Wres = cpdgen(Ures);
 time = toc;
 
 %% Tests
+% Train set
 Err = (frob(W)-frob(Wres))/frob(W)
-disp(['Relative error (tensor) frob norm: ',Err])
+disp(['Relative error (tensor) in train set: ',Err])
 
 %% Log file
-if exist('log.txt', 'file') ~= 2 % when file does not exist
-    fileID = fopen('log.txt','w');
-    formatSpec = ' Rel. error || Time (s) || Iterations || Stop info || Tensor order || Dimensions || Rank || Nonlin. f || Optimizer ||';
-    fprintf(fileID,formatSpec);
-    fclose(fileID);
-    
-    dimformat = string('%dx');
-    for ii=1:length(size(Wres))-1
-        dimformat = strcat(dimformat,'%dx');
-    end
-    
-    fileID = fopen('log.txt','a+');
-    formatSpec = strcat('\n %4.2f || %4.2f || %d || %d || %d || ',dimformat, ' || %d || %d || %10s');
-    fprintf(fileID,formatSpec,Err,time,output.iterations,output.info,order,size(Wres),rank,nonlin,optimizer);
-    fclose(fileID);
-    
-elseif exist('log.txt', 'file') == 2 % when file exists
-    fileID = fopen('log.txt','a+');
-    
-    % if I still want to write header when file exists...
-    formatSpec = '\n Rel. error || Time (s) || Iterations|| Stop info || Tensor order || Dimensions || Rank || Nonlin. f || Optimizer';
-    fprintf(fileID,formatSpec);
-    fclose(fileID);
-    
-    dimformat = string('%dx');
-    for ii=1:length(size(Wres))-1
-        dimformat = strcat(dimformat,'%dx');
-    end
-    
-    fileID = fopen('log.txt','a+');
-    formatSpec = strcat('\n %4.2f || %4.2f || %d || %d || %d || ',dimformat, ' || %d || %d || %10s');
-    fprintf(fileID,formatSpec,Err,time,output.iterations,output.info,order,size(Wres),rank,nonlin,optimizer);
-    fclose(fileID);
-    
-else % any other case
-    disp('*** Error in writing file ***')
-end
+% if exist('log.txt', 'file') ~= 2 % when file does not exist
+%     fileID = fopen('log.txt','w');
+%     formatSpec = ' Rel. error || Time (s) || Iterations || Stop info || Tensor order || Dimensions || Rank || Nonlin. f || Optimizer ||';
+%     fprintf(fileID,formatSpec);
+%     fclose(fileID);
+%     
+%     dimformat = string('%dx');
+%     for ii=1:length(size(Wres))-1
+%         dimformat = strcat(dimformat,'%dx');
+%     end
+%     
+%     fileID = fopen('log.txt','a+');
+%     formatSpec = strcat('\n %4.2f || %4.2f || %d || %d || %d || ',dimformat, ' || %d || %d || %10s');
+%     fprintf(fileID,formatSpec,Err,time,output.iterations,output.info,order,size(Wres),rank,nonlin,optimizer);
+%     fclose(fileID);
+%     
+% elseif exist('log.txt', 'file') == 2 % when file exists
+%     fileID = fopen('log.txt','a+');
+%     
+%     % if I still want to write header when file exists...
+%     formatSpec = '\n Rel. error || Time (s) || Iterations|| Stop info || Tensor order || Dimensions || Rank || Nonlin. f || Optimizer';
+%     fprintf(fileID,formatSpec);
+%     fclose(fileID);
+%     
+%     dimformat = string('%dx');
+%     for ii=1:length(size(Wres))-1
+%         dimformat = strcat(dimformat,'%dx');
+%     end
+%     
+%     fileID = fopen('log.txt','a+');
+%     formatSpec = strcat('\n %4.2f || %4.2f || %d || %d || %d || ',dimformat, ' || %d || %d || %10s');
+%     fprintf(fileID,formatSpec,Err,time,output.iterations,output.info,order,size(Wres),rank,nonlin,optimizer);
+%     fclose(fileID);
+%     
+% else % any other case
+%     disp('*** Error in writing file ***')
+% end
