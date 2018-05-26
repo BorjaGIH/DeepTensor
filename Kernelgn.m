7319d1e778f62f146c4e9fb5b5611518c938128a
BorjaDLT <velasco.6655@gmail.com>
2018-05-26 14:17:06 +0200

    WIP

--- Kernelgn.m
+++ Kernelgn.m
@@ -8,19 +8,21 @@ properties
     N       % order of the tensor
     R       % rank of the tensor for decomposition (constraint/efficient representation)
     resid   % residual, r(z), that is used in J, JHJ, etc.
-    gradient    % gradient, used in JHJx etc.
+    jacobian    % gradient, used in JHJx etc.
+    gramian
 end
 
 methods
 
-function this = Kernelgn(x, y, numfeat, N, R, resid, gradient) % constructor
+function this = Kernelgn(x, y, numfeat, N, R, resid, jacobian,gramian) % constructor
     this.x = x;
     this.y = y;
     this.numfeat = numfeat;
     this.N = N;
     this.R = R;
     this.resid = resid;
-    this.gradient = gradient;
+    this.jacobian = jacobian;
+    this.gramian = gramian;
 end
 
 function fval = objfun(this,z) % objective function
@@ -28,6 +30,7 @@ function fval = objfun(this,z) % objective function
     Y = this.y;
     npoints = size(X,1);
     Yest = zeros(npoints,1);
+    Xii = repmat(X,1,this.N);
     
     % Construct small matrix stacking rows
     for jj=1:this.R
@@ -42,7 +45,6 @@ function fval = objfun(this,z) % objective function
         end
     end
     
-    Xii = repmat(X,1,this.N);
     for ii=1:npoints % loop through all datapoints
         xii = Xii(ii,:)';
         tmp = m1*xii;
@@ -57,74 +59,140 @@ end
 function grad = grad(this,z) % column vector with the gradient
 %% analytic
     X = this.x;
-    Y = this.y;
     npoints = size(X,1);
     gradTmp = zeros(this.N*this.R*this.numfeat,1);
+    jacobtmp = zeros(this.N*this.R*this.numfeat,npoints);
+    tol = 5e-6;
     
     indx = repmat(1:this.numfeat,1,this.R*this.N);
     tmp = sort(repmat(1:this.R,1,this.N));
     rvec = repmat(tmp,1,this.N);
     nvec = sort(indx);
-        
+    
     for ii=1:npoints % loop through all datapoints
         
-        der = zeros(length(indx),1);
-        for jj=1:length(indx)
-            k = indx(jj);
-            n = nvec(jj);
-            r = rvec(jj);
-            
-            % Direct calculation
+        der = zeros(length(gradTmp),1);
+        for jj=1:length(gradTmp)
+            k = indx(jj); n = nvec(jj); r = rvec(jj);
             tmp2 = zeros(1,this.N);
             ztmp = z;
+            
+            % Direct calculation
+            ztmp{n}(:,r) = 0;
+            ztmp{n}(k,r) = 1;
+            
             for l=1:this.N
-                ztmp{n}(:,r) = 0;
-                ztmp{n}(k,r) = 1;
-                tmp2(l) = dot(ztmp{l}(:,r),X(ii,:));
+                tmp2(l) = ztmp{l}(:,r)'*X(ii,:)';
             end
-            der(jj) = prod(tmp2);
+            der(jj) = prod(tmp2);            
         end  
-        gradTmp = gradTmp + (this.resid(ii)).*der; % Can be written as multilinear op. (kron)
+        jacobtmp(:,ii) = der;
+        gradTmp = gradTmp + (this.resid(ii)).*der; 
     end
-    this.gradient = gradTmp;
+    this.jacobian = jacobtmp;
     grad = gradTmp;
+    gradjac = jacobtmp*this.resid;
+    err = frob(grad-gradjac)/grad;
+    if ~(err <= tol)
+        disp('Error 1');
+        return
+    end
+    grad = gradjac;
     
-    %% numerical
-%     grad1 = TensorOptimizationKernel.serialize(deriv(@this.objfun, z, this.objfun(z), 'gradient'));
-    % include assertion in case gradient fails
+        
+    %% Assert
+    grad1 = deriv(@this.objfun, z, this.objfun(z), 'gradient');
+    grad1 = TensorOptimizationKernel.serialize(grad1);
+    % computed
+    grad2 = gradTmp;
+    
+    % Check if correct
+    relerr = frob(grad1-grad2)/frob(grad1);
+
+%     assert(relerr <= tol);
+    if ~(relerr <= tol)
+        disp('Error 2');
+        return
+    end
+    grad = grad2;
     
 end
 
-function y = JHJx(this, x, ~) % CAREFUL! check nls_gndl line 371
-    gramian = this.gradient*this.gradient';
-    x = cell2mat(x);
-    y = gramian*x(:);
+function y = JHJx(this, z, x)
+    this.gramian = this.jacobian*this.jacobian';
+    y = this.gramian*x;
+    
+%     Jx = this.gradient'*x;
+%     y = this.gradient*Jx;
+%     J2 = this.jacobian;
+    
+    %% Assert
+% %     model = @(Z) objfun(this,Z);
+%     model = @(Z) residFun(this,z);
+% %     model = @(Z) cpdgen(Z);
+%     fun = 1;
+%     elementwise = isnumeric(fun) || nargin(fun) == 2; 
+%     tol = 1e-4;
+%     
+%     % target
+%     J1 = deriv(model, z, [], 'Jacobian');
+%     M  = reshape(model(z), [], 1);
+%     if isnumeric(fun)
+%         D1 = fun;
+%         if isvector(D1), D1 = diag(D1); end
+%     else
+%         D1 = TensorOptimizationKernel.numericHessianFD(fun, M, elementwise);
+%     end
+%     y1 = J1'*(D1*(J1*x));
+%     
+%     % computed
+%     y2 = y;
+%     
+%     % Check if correct
+%     relerr = frob(y1-y2)/frob(y1);
+%     if exist('assertElementsAlmostEqual.m', 'file')
+%         assertElementsAlmostEqual(relerr, 0, 'absolute', tol);
+%     else % fall back to Matlab's Unit Test Framework
+%         assert(relerr <= tol);
+%     end
+%     y = y2;
 end
 
-% function jhj = JHDJ(this, z)
-%     %JHDJ Compute an approximation of the Hessian.
-%     %   JHJ = JHDJ(Z) computes a positive semidefinite approximation of the Hessian
-%     %   of the objective function using variables Z. This approximation is
-%     %   usually the Gramian matrix, or its generalization to other
-%     %   divergences.
-%     
-%     
-% end
-% 
-% function y = JHDJx(this, z, x)
-%     %JHDJX Compute the approximate Hessian vector product.
-%     %   Y = JHDJX(Z,X) computes the product of the positive semidefinite
-%     %   approximation of the Hessian of the objective function constructed
-%     %   using variables Z, with a vector X. The approximation is usually the
-%     %   Gramian matrix, or its generalization to other divergences.
-%     
-%     
-% end
+function y = M_jacobi(this,z,q)
+    % Solve M*y = q, where M is an approximation for JHJ.
+    JHJinv = pinv(this.gramian);
+    y = JHJinv*q;
+end
 
-% function dF = setdF(this)
-%     dF.JHJx = @this.JHJx;
-%     dF.JHF = @this.grad;
-% end
+function res = residFun(this,z)
+    X = this.x;
+    Y = this.y;
+    npoints = size(X,1);
+    Yest = zeros(npoints,1);
+    Xii = repmat(X,1,this.N);
+    
+    % Construct small matrix stacking rows
+    for jj=1:this.R
+        b1 = [z{1}(:,jj)';zeros(this.N-1,this.numfeat)]; % block
+        for ii=2:this.N
+            b1 = [b1, [zeros(ii-1,this.numfeat);z{ii}(:,jj)';zeros(this.N-ii,this.numfeat)]]; % jj is the rank
+        end
+        if jj==1
+            m1 = b1; % matrix (M1)
+        else
+            m1 = [m1;b1];
+        end
+    end
+    
+    for ii=1:npoints % loop through all datapoints
+        xii = Xii(ii,:)';
+        tmp = m1*xii;
+        res = reshape(tmp,this.N,this.R);
+        Yest(ii) = sum(prod(res,1));
+    end
+    
+    res = Yest-Y;
+end
 
 function isvalid = validate(this, z)
     isvalid = true; % you can ignore this for now
--- nnTensor1.m
+++ nnTensor1.m
@@ -1,5 +1,5 @@
 clearvars; clc;
-rng(1);
+rng(89);
 % https://github.com/BorjaGIH/DeepTensor
 % % PD_constraint_dataTensor branch
 
@@ -19,10 +19,11 @@ facX = 1;                       % factor for the random datapoints
 
 optimizer = 'minf_lfbgs';  % optimizer and optimizer options
 options.Display = true;
-options.TolFun = eps;
+options.TolFun = eps^2;
 options.TolX = eps;
-options.MaxIter = 3000;
+options.MaxIter = 200;
 options.TolAbs = eps;
+options.CGmaxIter = 15;
 
 %% Generate data and tensors
 X = facX*rand(M,numfeat-1);            % X input
@@ -82,26 +83,21 @@ Xte = X(floor(ratioTr*size(X,1))+1:end,:);
 Ytr = Y(1:floor(ratioTr*size(Y,1)));
 Yte = Y(floor(ratioTr*size(Y,1))+1:end);
 
-%% Initial value
-% Initial value
-U0 = cpd_rnd(size_tens(:),R);            % random
-
 %% Optimization using kernel
 tic  % start time
-% Kernels
+
+U0 = cpd_rnd(size_tens(:),R);  % random initial value
+
 % kernel1 = Kernelbfgs(Xtr,Ytr,numfeat,N,R); % create kernel
 % kernel1.initialize(U0); % z0 is the initial guess for the variables, e.g., z0 = U0
 % [Uest,output] = minf_lbfgs(@kernel1.objfun, @kernel1.grad, U0, options); % Minimize
 
-kernel2 = Kernelgn(Xtr,Ytr,numfeat,N,R,[],[]); % create kernel
+kernel2 = Kernelgn(Xtr,Ytr,numfeat,N,R,[],[],[]); % create kernel
 kernel2.initialize(U0); % z0 is the initial guess for the variables, e.g., z0 = U0
-dF.JHJx = @kernel2.JHJx;
 dF.JHF = @kernel2.grad;
-[Uest,output] = nls_gndl(@kernel2.objfun, dF, U0);
-
-
-% Functions
-% [Uest,output] = nls_gndl(@objfun, 'Jacobian', U0);
+dF.JHJx = @kernel2.JHJx;
+% dF.M = @kernel2.M_jacobi;
+[Uest,output] = nls_gndl(@kernel2.objfun, dF, U0, options);
 
 time = toc;
 
@@ -127,6 +123,8 @@ if strcmp(generator,'tensor')
     ErrYte = norm(Yte-YestTe)/norm(Yte);    % error in output
     disp(['Relative error of Yest test, 2-norm: ',num2str(ErrYte)])
     
+    disp(['Time: ',num2str(time)])
+    
 elseif strcmp(generator,'function')
     % Plot of the error
     err = (sqrt(output.fval*2))/norm(Ytr);
@@ -144,7 +142,8 @@ elseif strcmp(generator,'function')
     YestTe = mtkrprod(West,Un,0)';
     ErrYte = norm(Yte-YestTe)/norm(Yte);
     disp(['Relative error of Yest test, 2-norm: ',num2str(ErrYte)])
-
+    
+    disp(['Time: ',num2str(time)])
 end
 
 %% Log file
@@ -182,72 +181,4 @@ end
 %     
 % else % any other case
 %     disp('*** Error in writing file ***')
-% end
-
-%% Functions
-% function fval = objfun(z) % objective function
-%     X = Xtr;
-%     Y = Ytr;
-%     npoints = size(X,1);
-%     Yest = zeros(npoints,1);
-%     
-%     % Construct small matrix stacking rows
-%     for jj=1:R
-%         b1 = [z{1}(:,jj)';zeros(this.N-1,this.numfeat)]; % block
-%         for ii=2:this.N
-%             b1 = [b1, [zeros(ii-1,this.numfeat);z{ii}(:,jj)';zeros(this.N-ii,this.numfeat)]]; % jj is the rank
-%         end
-%         if jj==1
-%             m1 = b1; % matrix (M1)
-%         else
-%             m1 = [m1;b1];
-%         end
-%     end
-%     
-%     Xii = repmat(X,1,this.N);
-%     for ii=1:npoints % loop through all datapoints
-%         xii = Xii(ii,:)';
-%         tmp = m1*xii;
-%         res = reshape(tmp,this.N,this.R);
-%         Yest(ii) = sum(prod(res,1));
-%     end
-%     
-%     this.resid = Yest-Y;
-%     fval = 0.5*(this.resid'*this.resid);
-% end 
-
-% function grad = grad(this,z) % column vector with the gradient
-% %% analytic
-%     X = this.x;
-%     Y = this.y;
-%     npoints = size(X,1);
-%     gradTmp = zeros(this.N*this.R*this.numfeat,1);
-%     
-%     indx = repmat(1:this.numfeat,1,this.R*this.N);
-%     tmp = sort(repmat(1:this.R,1,this.N));
-%     rvec = repmat(tmp,1,this.N);
-%     nvec = sort(indx);
-%         
-%     for ii=1:npoints % loop through all datapoints
-%         
-%         der = zeros(length(indx),1);
-%         for jj=1:length(indx)
-%             k = indx(jj);
-%             n = nvec(jj);
-%             r = rvec(jj);
-%             
-%             % Direct calculation
-%             tmp2 = zeros(1,this.N);
-%             ztmp = z;
-%             for l=1:this.N
-%                 ztmp{n}(:,r) = 0;
-%                 ztmp{n}(k,r) = 1;
-%                 tmp2(l) = dot(ztmp{l}(:,r),X(ii,:));
-%             end
-%             der(jj) = prod(tmp2);
-%         end  
-%         gradTmp = gradTmp + (this.resid(ii)).*der; % Can be written as multilinear op. (kron)
-%     end
-%     grad = gradTmp;
-%     
 % end
